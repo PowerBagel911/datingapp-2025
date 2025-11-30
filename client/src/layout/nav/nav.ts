5b9ade468557728630af2097035d83e745947192
Duc Tri Tran <105089392@student.swin.edu.au>
2025-11-30 19:41:55 +1100

    implementing the custom toast

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { inject } from '@angular/core';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
@@ -13,15 +14,19 @@ import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 export class Nav {
   protected accountService = inject(AccountService)
   protected router = inject(Router);
+  private toast = inject(ToastService);
   protected creds: any = {}
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: result => {
         this.router.navigateByUrl('/members');
+        this.toast.success('Logged in successfully!');
         this.creds = {}; // Clear credentials after successful login
       },
-      error: error => alert(error.message)
+      error: error => {
+        this.toast.error(error.error);
+      }
     });
   }
 
