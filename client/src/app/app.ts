1144d6f1c8282b8178356d1e21824d2a29d0fe64
Duc Tri Tran <105089392@student.swin.edu.au>
2025-12-02 14:30:01 +1100

    end of section 6

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,44 +1,14 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 import { Router, RouterOutlet } from '@angular/router';
-import { NgClass } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, RouterOutlet, NgClass],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html', // Considered a child of this file
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  
-  private accountService = inject(AccountService);
+export class App {
   protected router = inject(Router);
-  private http = inject(HttpClient);
-  protected readonly title = 'Dating App';
-  protected members = signal<User[]>([]); // a property to store fetched users
-
-  ngOnInit(): void {
-    this.http.get<User[]>('https://localhost:5001/api/members').subscribe({
-      next: response => this.members.set(response),
-      error: error => console.log(error),
-      complete: () => console.log('Completed the http request.')
-    })
-
-    this.setCurrentUser();  // ⬅️ First thing: restore login state
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) {
-      return;
-    }
-
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
 }
