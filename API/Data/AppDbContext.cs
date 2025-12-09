4eae87e71b4daf0aedc28220de90f29813fadb78
Duc Tri Tran <105089392@student.swin.edu.au>
2025-12-09 14:49:05 +1100

    add Member and Photo entities; update AppDbContext to include new DbSets

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -9,4 +9,7 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     public DbSet<AppUser> Users { get; set; }
     //     ↑              ↑
     //  Collection     Property name = Table name
+
+    public DbSet<Member> Members { get; set; }
+    public DbSet<Photo> Photos { get; set; }
 }
